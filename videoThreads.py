ac22764a5c0845bf87b4642bad308b8314a6d630
Crystal <cserrano5@miners.utep.edu>
2020-11-09 16:00:12 -0700

    updated queue class and merged into videoThreads file, fixed videoThreads and now all threads are working

--- videoThreads.py
+++ videoThreads.py
@@ -1,94 +1,140 @@
-#!/usr/bin/env python3
-
-import cv2, os, sys, time
-from Queue import Queue
-import numpy as np
+#! /usr/bin/python3
+import cv2, time
 from threading import Thread, Semaphore, Lock
 
-
-
-
-#create queues
-frameQueue = Queue() 
-grayQueue = Queue() 
-
-
-
-
-#extracting frame thread class
-class ExtractFramesThread(Thread):
+#producer and consumer queue class
+class Queue():
     def __init__(self):
-        Thread.__init__(self)
-        self.videocap = cv2.VideoCapture('clip.mp4')                            #this opens video clip
-        self.totalFrames = int(self.videocap.get(cv2.CAP_PROP_FRAME_COUNT))
-        self.count = 0
+        self.queue = []   #initializes a queue
+        
+        # two semaphores and a mutex to use per each queue
+        self.first = Semaphore(10) # max ten frames per queue
+        self.second = Semaphore(0) # waits til other thread calls release() 
+        self.mutex = Lock()
+        
+    def put(self, frame):
+        # acquiere first sempahore which would be realesed by another thread when consuming from the queue
+        self.first.acquire()
+        self.mutex.acquire()
+        self.queue.append(frame)
+        self.mutex.release()
+        self.second.release() # if 10 has been acquiered, it will wait until consumers realese a permit
+        return
+    
+    def get(self):
+        # acquiere second Semaphore which would be realesed by another thread when producing to the queue
+        self.second.acquire()
+        self.mutex.acquire()
+        frame = self.queue.pop(0)
+        self.mutex.release()
+        self.first.release() # realese 1 permit that we use to add a frame to the first queue
+        return frame
+    
+    
+# create two queues for the frames
+frameQueue = Queue()
+grayQueue = Queue()
 
-    def run(self):                                                          #this runs code
-        success, image = self.videocap.read()                                 #this reads first frame
+
+class ExtractFrames(Thread):
+    def __init__(self):  
+        Thread.__init__(self)
+        self.videoCapture = cv2.VideoCapture('clip.mp4')
+        # get total frames of the video
+        self.totalFrames = int(self.videoCapture.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.count = 0
+        
+    def run(self):
+        # gets the first frame and a boolean value if frame extraccion is successful
+        success, image = self.videoCapture.read()
+
         while True:
-            if success:
-                frameQueue.put(image) #gets frame
-                success, image = self.videocap.read()
+            # if frame extraccion is successful, and continues to extract the next frame
+            if success: 
+                # get the frame
+                frameQueue.put(image)
+                # get next frame from file if the extraccion is successful
+                success, image = self.videoCapture.read() 
                 print(f'Reading frame {self.count}')
                 self.count = self.count + 1
-                
+
+            # once we reach the total frame count, we add -1 to signal the threads that there are no more frames
             if self.count == self.totalFrames:
                 frameQueue.put(-1)
-                break
+                break  
             
-        print('Extraction is complete.')
-      
+        # signal to notify that thread has finished   
+        print('Frame extraction complete')
+        return
+            
+class ConvertToGrayScale(Thread):
+        def __init__(self):
+            Thread.__init__(self)
+            self.count = 0
+            
+        def run(self):
+           
+            while True:
+                # get a frame from the first queue
+                frame = frameQueue.get()
 
-#convert to gray scale thread class
-class ConvertToGrayScaleThread(Thread):
-    def __init__(self):
-        Thread.__init__(self)
-        self.count = 0
-
-    def run(self):                                                          #this runs code
-        while True:
-            frame = frameQueue.get()
-            if type(frame) == int and frame == -1:
-                grayQueue.put(-1)
-                break
-            grayscaleFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            grayQueue.put(grayscaleFrame)
+                # if we see the signal (-1) that there are no more frames, we exit the loop
+                if type(frame) == int and frame == -1:
+                    grayQueue.put(-1)
+                    break
                 
-            print('Gray scale convertion complete')
+                # converts normal frame to grayscale and add it to the second queue
+                grayscaleFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                grayQueue.put(grayscaleFrame)
+                
+                print(f'Converting Frame {self.count}')
+                self.count = self.count + 1
+            
+            # signal to notify that thread is finished    
+            print('Convert to gray scale complete')
             return
-        
-        
-#display frame thread class
-class DisplayFrameThread(Thread):
+
+class DisplayVideo(Thread):
     def __init__(self):
         Thread.__init__(self)
-        self.delay = 42
-        self.count = 0
         
+        self.delay = 42 #delay of 42 ms
+        self.count = 0
 
     def run(self):
         while True:
+            # get a grayscale frame
             frame = grayQueue.get()
+
+            # if we see the signal (-1) that there are no more frames, we break the loop
             if type(frame) == int and frame == -1:
                 break
+
+            # display grayscale frame
             cv2.imshow('Video', frame)
-            print(f'Displaying frame {self.count}')
-            self.count = self.count + 1
             
+            print(f'Displaying Frame {self.count}')
+            self.count = self.count + 1
+
+            # wait 42ms before displaying the next frame
             if cv2.waitKey(self.delay) and 0xFF == ord('q'):
                 break
-            
+                    
+        #destory all video screen
         cv2.destroyAllWindows()
         
-        print('Frame display complete')  #signal threas has ended
+        # signal to notify that thread is finished    
+        print('Display frames complete')
         return
-        
-    
-extractFrame = ExtractFramesThread()   #extract thread created
-extractFrame.start()
 
-convertGray = ConvertToGrayScaleThread()  #convert to gray thread created
-convertGray.start()
 
-displayFrame = DisplayFrameThread()    #display frame thread created
-displayFrame.start()
+# create and start all threads
+
+extractFrames = ExtractFrames()
+extractFrames.start()
+
+convertFrames = ConvertToGrayScale()
+convertFrames.start()
+
+displayFrames = DisplayVideo()
+displayFrames.start()
